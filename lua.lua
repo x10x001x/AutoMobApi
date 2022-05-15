b41f5dee4a4c15d422581250f71f50c340687b0e
x10x001x <82150516+x10x001x@users.noreply.github.com>
2022-05-15 11:04:15 +0200

    Create lua.lua

---
+++ lua.lua
@@ -0,0 +1,25 @@
+local AbilityTokens = {1442700745,1472256444}
+function GoInvis()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(0,-1,0))
+task.wait()
+local Root = game.Players.LocalPlayer.Character.LowerTorso.Root
+local Clone = Root:Clone()
+Root:Destroy()Clone.Parent = game.Players.LocalPlayer.Character.LowerTorso
+task.wait()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-114.044, 2.30161, 271.535))
+end
+function CollectTokens(enable)
+while task.wait(.1) and enable do
+if enable == true then
+for _,v in pairs(game:GetService("Workspace").Collectibles:GetChildren()) do
+decal = v:FindFirstChildOfClass("Decal")
+for i,z in next, AbilityTokens do
+if decal.Texture == "rbxassetid://"..z then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.Position.X,v.Position.Y,v.Position.Z)
+break
+                    end
+                end
+            end
+        end
+    end
+end
